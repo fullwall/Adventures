503002527423277e8c03114641a10e396c9af007
fullwall <fullwall25@gmail.com>
2012-12-24 23:44:59 +0800

    Test

--- src/main/java/net/citizensnpcs/adventures/goap/npc/ThreatSensor.java
+++ src/main/java/net/citizensnpcs/adventures/goap/npc/ThreatSensor.java
@@ -1,7 +1,6 @@
 package net.citizensnpcs.adventures.goap.npc;
 
 import java.util.Collection;
-import java.util.Collections;
 
 import net.citizensnpcs.adventures.goap.PlannerAgent;
 import net.citizensnpcs.adventures.goap.Sensor;
@@ -12,6 +11,7 @@ import org.bukkit.entity.Monster;
 import org.bukkit.entity.Player;
 
 import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 
 public class ThreatSensor implements Sensor {
@@ -28,18 +28,20 @@ public class ThreatSensor implements Sensor {
         if (hasThreat)
             state.put("threats", filtered);
         else
-            state.put("threats", Collections.EMPTY_LIST);
+            state.put("threats", EMPTY);
         return state;
     }
 
     public Collection<Entity> getThreats() {
         Collection<Entity> threats = state.get("threats");
         if (threats == null)
-            return Collections.emptyList();
+            return EMPTY;
         return threats;
     }
 
     public boolean hasThreats() {
         return state.get("hasThreat");
     }
+
+    private static final Collection<Entity> EMPTY = Lists.newArrayList();
 }
