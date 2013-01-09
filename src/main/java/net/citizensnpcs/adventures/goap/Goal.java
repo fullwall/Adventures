0365723302ef7911dd45694569ec63de73a79ff0
fullwall <fullwall25@gmail.com>
2013-01-09 23:25:32 +0800

    Remove GOAP

--- src/main/java/net/citizensnpcs/adventures/goap/Goal.java
+++
@@ -1,9 +0,0 @@
-package net.citizensnpcs.adventures.goap;
-
-public interface Goal {
-    boolean canContinue();
-
-    float evaluateRelevancy(PlannerAgent agent);
-
-    WorldState getGoalState();
-}
--- src/main/java/net/citizensnpcs/adventures/sensors/Sensor.java
+++
@@ -1,7 +0,0 @@
-package net.citizensnpcs.adventures.sensors;
-
-import net.citizensnpcs.adventures.goap.WorldState;
-
-public interface Sensor {
-    WorldState generateState();
-}
