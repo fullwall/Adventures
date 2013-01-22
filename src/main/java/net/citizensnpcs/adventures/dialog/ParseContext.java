64ecf81d643d44347a0387e0c0ef38573f60d17f
fullwall <fullwall25@gmail.com>
2013-01-22 23:31:51 +0800

    Delete files

--- src/main/java/net/citizensnpcs/adventures/dialog/ParseContext.java
+++
@@ -1,9 +0,0 @@
-package net.citizensnpcs.adventures.dialog;
-
-import java.util.Collection;
-
-public interface ParseContext {
-    void responseLoaded(Response response);
-
-    void ruleLoaded(Collection<String> eventNames, Rule rule);
-}
