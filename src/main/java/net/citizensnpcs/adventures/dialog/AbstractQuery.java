fa4b50de2fd4af170a5055333d3bd48e56bf8de3
fullwall <fullwall25@gmail.com>
2013-02-04 11:18:35 +0800

    Implement reflective property accessing

--- src/main/java/net/citizensnpcs/adventures/dialog/AbstractQuery.java
+++ src/main/java/net/citizensnpcs/adventures/dialog/AbstractQuery.java
@@ -1,7 +1,12 @@
 package net.citizensnpcs.adventures.dialog;
 
+import java.lang.reflect.Method;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 
 public abstract class AbstractQuery implements Query {
     private final String eventName;
@@ -26,8 +31,61 @@ public abstract class AbstractQuery implements Query {
 
     @Override
     public Object get(String key) {
-        return queryVariables.get(key);
+        Object quickPath = queryVariables.get(key);
+        if (quickPath != null)
+            return quickPath;
+        return slowGet(key);
     }
+
+    private Object slowGet(String key) {
+        int dotIndex;
+        Object current = null;
+        List<String> parts = Lists.newArrayListWithCapacity(4);
+        while (current == null && (dotIndex = key.lastIndexOf('.')) != -1) {
+            parts.add(key.substring(dotIndex + 1));
+            key = key.substring(0, dotIndex);
+            current = queryVariables.get(key);
+        }
+        if (current == null)
+            return null;
+        for (String part : parts) {
+            key += part;
+            Method cached = METHOD_CACHE.get(key);
+            if (cached == null)
+                cached = findGetterMethod(current, part);
+            if (cached == null)
+                return null;
+            try {
+                current = cached.invoke(current, (Object[]) null);
+            } catch (Exception e) {
+                return null;
+            }
+        }
+        return current;
+    }
+
+    private Method findGetterMethod(Object source, String methodName) {
+        Class<?> sourceClass = source.getClass();
+        Method test = getMethod(sourceClass,
+                "get" + Character.toUpperCase(methodName.charAt(0)) + methodName.substring(1));
+        if (test != null)
+            return test;
+        test = getMethod(sourceClass, methodName);
+        return test;
+    }
+
+    private Method getMethod(Class<?> source, String name) {
+        Method ret = null;
+        try {
+            ret = source.getMethod(name, (Class<?>[]) null);
+        } catch (Exception e) {
+        }
+        if (ret != null)
+            ret.setAccessible(true);
+        return ret;
+    }
+
+    private static final Map<String, Method> METHOD_CACHE = Maps.newHashMap();
 
     @Override
     public String getEventName() {
